426ab02d0c43a18c2ecd2b61a192382e88d42fad
Ethan Lynn <ethan@sabado.com>
2019-10-10 14:58:28 -0400

    Launch screen.

--- isitlit/App.js
+++ isitlit/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import fire from './assets/fire.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is starter text</Text>
+      <Image source={fire} style={{ width: 100, height: 100, marginBottom: 10 }} />
+      <Text style={styles.title}>Is It Lit?</Text>
     </View>
   );
 }
@@ -12,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f80',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 48,
   },
 });
