4ee5f269610635643c1d05aef2292eb49018e47d
Ethan Lynn <ethan@sabado.com>
2019-10-10 15:10:02 -0400

    Added button/map.

--- isitlit/App.js
+++ isitlit/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { View, Image } from 'react-native';
 import fire from './assets/fire.png';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source={fire} style={{ width: 100, height: 100, marginBottom: 10 }} />
-      <Text style={styles.title}>Is It Lit?</Text>
-    </View>
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <View style={{ backgroundColor: '#f80', alignItems: 'center', height: 140 }}>
+        <Image source={fire} style={{ width: 100, height: 100, margin: 20 }} />
+      </View>
+   </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f80',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 48,
-  },
-});
